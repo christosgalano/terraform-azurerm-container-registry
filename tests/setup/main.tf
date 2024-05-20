afb372c1e5f0f5207db26901989598c562340c35
Christos Galanopoulos <christosgalanop@gmail.com>
2024-05-20 10:35:21 +0300

    Update content

--- tests/setup/main.tf
+++ tests/setup/main.tf
@@ -17,12 +17,12 @@ resource "random_pet" "name" {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
+  name     = "${var.resource_group_name}-${random_pet.name.id}"
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = "${var.vnet_name}-${random_pet.name.id}"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   address_space       = ["10.0.0.0/24"]
